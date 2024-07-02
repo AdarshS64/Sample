e2ca2f1063a4b0ca5fd5ad41754053431f7da486
AdarshS64 <adarshmeant4gr8@gmail.com>
2024-07-02 22:30:46 +0530

    feat: Created basic backend with ts

---
+++ app.ts
@@ -0,0 +1,83 @@
+//version 1
+
+// const http = require("http");
+// // created an http which is stored in the constant
+
+// const server = http.createServer((req, res) => {
+//   console.log("hello");
+//   console.log(req.url);
+//   res.writeHead(200);
+//   res.end("hi"); //to send response to client
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+//multiple endpoints can do if case for req.url but thats clutter so a package is used called Express
+
+//npm init -y this is my file for stuf do the necessary
+
+// version 2 using express
+
+
+
+/* const { error } = require("console");
+const express = require("express");
+
+const server = new express(); */
+import { log } from "console";
+import {Request,Response} from "express";
+import express from "express";
+const server =express()
+
+server.get("/", (Request:Request, Response:Response) => {
+//   const data : = {
+//     name: "Champ",
+//     age: 20,
+//   };
+  interface Profile{
+    name: string,
+    age: number
+  }
+
+  interface data{
+    profile: Profile
+  }
+
+  let data:data = {
+    profile: {
+      name: "Champ",
+      age: 20,
+    },
+  }; 
+
+  // you can also do this
+  // let data:{profile: Profile} = {
+  //   profile: {
+  //     name: "Champ",
+  //     age: 20,
+  //   },
+  // }; 
+
+  
+
+//   this showed the need of type script
+  console.log(Request.url + "car");
+  console.log(data.profile.name);
+  Response.status(200).send(data);
+});
+
+server.get("/heyy", (req, res) => {
+  try {
+    log(req.url);
+    throw new Error();
+    res.status(200).send("Ma name is Jeff");
+  } catch (error) {
+    res.status(200).send("heyyy");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ average.js
@@ -0,0 +1,8 @@
+function average(a,b){
+    a=parseFloat(a);
+    b=parseFloat(b);
+    return 'abc';
+
+}
+
+console.log('average',average(1.04,3.4));
